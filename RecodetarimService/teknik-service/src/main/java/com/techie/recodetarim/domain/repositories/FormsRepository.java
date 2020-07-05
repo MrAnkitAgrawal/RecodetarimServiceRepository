f3203c7a31717833305bfd15f88fe04b30eb1c40
MrAnkitAgrawal <agrawal.ank83@gmail.com>
2020-07-05 12:56:40 +0200

    remiving unwanted files

--- RecodetarimService/teknik-service/src/main/java/com/techie/recodetarim/domain/repositories/FormsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.techie.recodetarim.domain.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.techie.recodetarim.domain.entities.Forms;
-
-public interface FormsRepository extends JpaRepository<Forms, Long> {
-}
